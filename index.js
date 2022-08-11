69d1c829c07f47c02fed4f4963c8ff0028eade5b
Alle_rayane <alequissandrarayane123@gmail.com>
2022-08-11 13:46:26 -0300

    .

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
+//função onclick
 function consultarRacas(){
     let racas = document.getElementById('racas').value;
- 
+
+    //se não for digitado nada, irá disparar o Alert na tela
     if(racas === "" ){
          alert ('Digite um nome de Raça váliada!');
          return;
      }
- 
-     //fazer a concatenação se não func.
+     
+        //consulta o url da api e exibe a img. do cachorro na tela
        let url ="https://dog.ceo/api/breed/"+ racas + "/images/random"
- 
          fetch(url).then( function(response){
              response.json().then(function(data){
              console.log(data)
              mostraRacas(data)
+             let frase = document.getElementById('frase');
+             frase.innerHTML = "&#127881;&#128054;Aqui está a foto da raça do doguinho que você pesquisou.&#128512; Ligue agora para nossa instituição e agende sua vista!";
          })
      })
- 
- 
+    
+   
  }
- 
- 
  
  function mostraRacas(data){
      let resultado = document.getElementById('resultado');
- 
      resultado.innerHTML = `<img src="${data.message}"/>` 
-                        
-                          
+     
+                           
  }
 
